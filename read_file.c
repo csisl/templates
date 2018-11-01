32cc7917591a4206ac0a56e6609901a657ea3ef0
Stephanie Callahan <31281102+stncal@users.noreply.github.com>
2018-11-01 16:13:56 -0500

    Code for reading files in C

---
+++ read_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void readFile(char *fileName){
+	FILE *fp;
+	char buf[255];
+	
+	fp = fopen(fileName, "r");
+	
+	if(fp == NULL){
+		fprintf(stderr, "Could not open file %s\n", fileName);
+		exit(-1);
+	}
+	
+	while(fgets(buf, 255, fp) != NULL){
+		printf("%s", buf);
+	}
+	
+	fclose(fp);
+}
+
+int main(int argc, char *argv[]){
+
+	if(argc < 2){
+		printf("Invalid number of arguments\n");
+		exit(-1);
+	}
+	
+	readFile(argv[1]);
+
+	return 0;
+}
