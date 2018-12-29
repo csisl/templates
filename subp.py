9795b13c1dccbcd2c7e55b46b923a1cdf598b5f7
Stephanie Callahan <31281102+stncal@users.noreply.github.com>
2018-12-28 23:48:14 -0600

    Create subp.py

---
+++ subp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+import subprocess
+
+# usage: python subp.py
+# This script will execute the program and check the output for a certain keyword
+
+# script to be executed
+script = ""
+
+# check the ouput for certain keywords
+output = subprocess.check_output("./{}".format(script), shell=True)
+
+# keyword to check for 
+keyword = ""
+
+if keyword in output:
+	# the output we are looking for was found
+	print("keyword found: {}".format(keyword))
