f12d2ee7b9236e332eac3001320f59e7b2d88d4d
Stephanie Callahan <31281102+stncal@users.noreply.github.com>
2018-12-31 09:36:34 -0600

    Update sub_call.py

--- sub_call.py
+++ sub_call.py
@@ -1,26 +1,30 @@
-  1 #!/usr/bin/python3
-  2 import subprocess
-  3 import sys
-  4 from pathlib import Path    # to make sure the file exists before executing
-  5
-  6 # script to be executed
-  7 script = ""
-  8
-  9 # how to execute the script; default is ./
- 10 exc = "./"
- 11
- 12 # ensure the script file exists
- 13 # default path is the pwd
- 14 path = "./" + script
- 15
- 16 path_check = Path(path)
- 17 if not path_check.is_file():
- 18     print("File not found {}".format(path))
- 19     sys.exit()
- 20
- 21 try:
- 22     executable = exc + script
- 23     # attempt to execute the script
- 24     subprocess.call([executable])
- 25 except:
- 26     print("Unable to execute {}".format(script))
+#!/usr/bin/python3
+import subprocess
+import sys
+from pathlib import Path    # to make sure the file exists before executing
+
+# template for executing another script with a python program using subprocess.call
+
+# usage: python sub_call.py
+
+# script to be executed
+script = ""
+
+# how we want to execute the other script; default is ./
+exc = "./"
+
+# ensure the script exists before continuing
+# default path is the pwd
+path = "./" + script
+
+path_check = Path(path)
+if not path_check.is_file():
+    print("File not found {}".format(path))
+    sys.exit()
+    
+try:
+    executable = exc + script
+    # attempt to execute the script
+    subprocess.call([executable])
+except:
+    print("Unable to execute {}".format(script))
